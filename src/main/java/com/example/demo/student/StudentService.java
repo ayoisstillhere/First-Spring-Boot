4cd67ff2969497a32004db89d7415622e1be301c
Ayodele Fagbami <afagbami@gmail.com>
2022-06-27 22:04:22 +0100

    return students from database using repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,29 +1,21 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return  List.of(
-                new Student(
-                        1L,
-                        "Ayodele",
-                        "afagbami@gmail.com",
-                        LocalDate.of(2002, Month.AUGUST,26),
-                        19
-                ),
-                new Student(
-                        2L,
-                        "Mildred",
-                        "millie@gmail.com",
-                        LocalDate.of(2002, Month.JANUARY,21),
-                        20
-                )
-        );
+        return  studentRepository.findAll();
     }
 }
