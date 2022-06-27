1d2e01d3a9e804168bb210a20f71e5b587f317a1
Ayodele Fagbami <afagbami@gmail.com>
2022-06-27 11:38:48 +0100

    returned list of student objects

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,23 @@ public class DemoApplication {
     }
 
     @GetMapping
-    public List<String> hello() {
-        return  List.of("Hello", "World");
+    public List<Student> hello() {
+        return  List.of(
+                new Student(
+                        1L,
+                        "Ayodele",
+                        "afagbami@gmail.com",
+                        LocalDate.of(2002, Month.AUGUST,26),
+                        19
+                ),
+                new Student(
+                        2L,
+                        "Mildred",
+                        "millie@gmail.com",
+                        LocalDate.of(2002, Month.JANUARY,21),
+                        20
+                )
+        );
     }
 
 }
